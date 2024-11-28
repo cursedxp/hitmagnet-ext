098ab2ba2ea3de22e12929a231cf4ccf549ac333
Anil Ozsoy <anilozsoy@gmail.com>
2024-11-28 15:54:51 +0100

    🔧 (vite.config.js): add support for VITE_GOOGLE_CLIENT_ID environment variable to be able to use Google OAuth in the app

--- vite.config.js
+++ vite.config.js
@@ -35,6 +35,7 @@ export default defineConfig(({ mode }) => {
     ],
     define: {
       "import.meta.env": {
+        VITE_GOOGLE_CLIENT_ID: process.env.VITE_GOOGLE_CLIENT_ID,
         VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
         VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
         VITE_FIREBASE_PROJECT_ID: process.env.VITE_FIREBASE_PROJECT_ID,
